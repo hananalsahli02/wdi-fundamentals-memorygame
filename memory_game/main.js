917372a2268647a0fccb651a8a98b19438a15351
hananalsahli02 <hananalsahli03@gmail.com>
2020-02-09 00:08:43 +0000

    created game logic

---
+++ memory_game/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+let cardOne = "queen";
+let cardtow = "queen";
+let cardcardThree="king";
+let cardcardFour="king";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardcardThree);
