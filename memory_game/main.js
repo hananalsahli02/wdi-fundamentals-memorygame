d4ca42a9458dd6ba1884113578626b6d857f96fe
hananalsahli02 <hananalsahli03@gmail.com>
2020-02-10 00:32:43 +0000

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -5,20 +5,21 @@ console.log("Up and running!");
 alert('Hello, friends.');
 const cards = ["queen", "queen", "king","king"];
 const cardsInPlay = [];
-var cardOne ;
-var cardtow ;
-cardOne = cards [0];
-cardsInPlay.push (cardOne);
-console.log("user flipped queen "+ cardOne);
+var flipCard = function(cardId) {
+  console.log("User  flipped" +cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+}
+flipCard(0);
+flipCard(2);
 
-cardtow = cards [2];
-cardsInPlay.push (cardtow);
-console.log("User flipped king"+ cardtow);
-if (cardsInPlay.length ===2){
-  if (cardsInPlay [0]=== cardsInPlay[1]){
-    alert("You found a match!");
-  }
-  else {
-    alert("Sorry, try again.");
-  }
+var checkFormatch = function() {
+  if (cardsInPlay[0] ===cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
+if (cardsInPlay.length ===2)
+{
+  checkFormatch();
 }
